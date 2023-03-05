c68d2a0757afa77be507911da56c00e9fb4a9e7c
Amit Deshmukh <shivpatil308@gmail.com>
2023-03-05 15:05:19 +0530

    test

---
+++ src/component/fetchDeta.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const FetchDeta = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => console.log(posts)}>GET DATA</button>
+      <br></br>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FetchDeta;
